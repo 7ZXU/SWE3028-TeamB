0f11defa4a4d6623c8561613d54939d1cf5c7138
angbuilgu <sksmsdlscks@gmail.com>
2021-10-24 23:00:54 +0900

    test2

--- src/components/Days.js
+++ src/components/Days.js
@@ -3,14 +3,20 @@ import {MdCheckBox,MdCheckBoxOutlineBlank} from "react-icons/md";
 import "./Days.css"
 
 const Days = ({day}) => {
-    const {id,done,checked} = day;
+    const {id,done,checked,state} = day;
+    
     return (
         <div className="ChallengeDays">
         <div className={'content ${checked ? "checked" : ""}'}>
-            {done ? <MdCheckBox size="30"/> : <MdCheckBoxOutlineBlank size="30"/>}
+        {state && done && checked && <MdCheckBoxOutlineBlank size="30" color="green"/>}
+        {!state && <MdCheckBoxOutlineBlank size="30" color="gray"/>}
+        {state && !checked && <MdCheckBoxOutlineBlank size="30" color="yellow"/>}
+        {state && checked &&! done && <MdCheckBoxOutlineBlank size="30" color="red"/>}
         </div>
         </div>
     );
 };
 
 export default Days;
+
+/*{done ? <MdCheckBox size="30"/> : <MdCheckBoxOutlineBlank size="30"/>} */
